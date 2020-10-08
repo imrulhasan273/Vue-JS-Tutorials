dfcd8633d28e0b93a9ee865472f01dcf97f26bfa
Md. Imrul Hasan <imrulhasan273@gmail.com>
2020-10-08 21:57:57 +0600

    readme

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
-Vue.component("greeting", {
-  template:
-    "<p>re-usable component. {{name}}. <button v-on:click='changeName'>Change Name</button> </p>",
-
-  data: function () {
-    return {
-      name: "Imrul",
-    };
-  },
-
-  methods: {
-    changeName: function () {
-      this.name = "Hasan";
-    },
-  },
-});
-
-/* new Vue({
-    el: '.test',
-    template: '<p>I am a template</p>'
-}); */
-
 new Vue({
-  el: "#vue-app-one",
-});
-
-new Vue({
-  el: "#vue-app-two",
+  el: "#vue-app",
+  data: {},
+  methods: {},
+  computed: {},
 });
