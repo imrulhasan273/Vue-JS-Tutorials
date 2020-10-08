4b1e24a0d12eb329f90e89313f384f8d862ad437
Md. Imrul Hasan <imrulhasan273@gmail.com>
2020-10-08 21:52:57 +0600

    n

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
+Vue.component("greeting", {
+  template:
+    "<p>re-usable component. {{name}}. <button v-on:click='changeName'>Change Name</button> </p>",
+
+  data: function () {
+    return {
+      name: "Imrul",
+    };
+  },
+
+  methods: {
+    changeName: function () {
+      this.name = "Hasan";
+    },
+  },
+});
+
+/* new Vue({
+    el: '.test',
+    template: '<p>I am a template</p>'
+}); */
+
 new Vue({
-  el: "#vue-app",
-  data: {},
-  methods: {},
+  el: "#vue-app-one",
+});
+
+new Vue({
+  el: "#vue-app-two",
 });
